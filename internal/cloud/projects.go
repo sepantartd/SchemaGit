5c050030c97b286ffb3a51be674ae6338be3890c
Sepanta <spnta40@gmail.com>
2026-09-23 01:45:46 +0330

    Update projects.go

--- internal/cloud/projects.go
+++ internal/cloud/projects.go
@@ -2,28 +2,12 @@ package cloud
 
 import "github.com/sepanta/schemagit/internal/store"
 
-var projStore *store.Store
-
-func InitProjects(path string) {
-    st, err := store.Open(path)
-    if err != nil {
-        return
+func GetProjectByID(id int64) *store.Project {
+    projects := GetProjects()
+    for _, p := range projects {
+        if p.ID == id {
+            return &p
+        }
     }
-    projStore = st
-}
-
-func GetProjects() []store.Project {
-    if projStore == nil {
-        return []store.Project{}
-    }
-    return projStore.LoadProjects()
-}
-
-func AddProject(name string) {
-    if projStore == nil {
-        return
-    }
-    projStore.SaveProject(store.Project{
-        Name: name,
-    })
+    return nil
 }
