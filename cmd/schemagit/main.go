13c116a62d5ea56e7abab8656ac18e223e53b111
Sepanta <spnta40@gmail.com>
2026-09-22 20:26:35 +0330

    Update help message and version in main.go

--- cmd/schemagit/main.go
+++ cmd/schemagit/main.go
@@ -8,27 +8,43 @@ import (
     "github.com/sepanta/schemagit/internal/log"
 )
 
+func printHelp() {
+    fmt.Println("SchemaGit v0.2")
+    fmt.Println("")
+    fmt.Println("Usage:")
+    fmt.Println("  schemagit diff      Show differences between DB and desired schema")
+    fmt.Println("  schemagit apply     Apply schema changes to database")
+    fmt.Println("")
+    fmt.Println("Examples:")
+    fmt.Println("  schemagit diff")
+    fmt.Println("  schemagit apply")
+}
+
 func main() {
     if len(os.Args) < 2 {
-        fmt.Println("SchemaGit v0.1")
-        fmt.Println("Usage:")
-        fmt.Println("  schemagit diff")
-        fmt.Println("  schemagit apply")
+        printHelp()
         return
     }
 
     cmd := os.Args[1]
 
     switch cmd {
+
     case "diff":
         if err := cli.RunDiff(); err != nil {
             log.Error(err.Error())
         }
+
     case "apply":
         if err := cli.RunApply(); err != nil {
             log.Error(err.Error())
         }
+
+    case "help":
+        printHelp()
+
     default:
         log.Error("Unknown command: " + cmd)
+        printHelp()
     }
 }
