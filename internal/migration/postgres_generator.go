e86b3f204809c80fcd65ccf37eb6094109bec32c
Sepanta <spnta40@gmail.com>
2026-09-22 20:22:44 +0330

    Create postgres_generator.go

---
+++ internal/migration/postgres_generator.go
@@ -0,0 +1,43 @@
+package migration
+
+import (
+    "fmt"
+
+    "github.com/sepanta/schemagit/internal/diff"
+    "github.com/sepanta/schemagit/internal/schema"
+)
+
+func GeneratePostgresSQL(d *diff.SchemaDiff) ([]string, error) {
+    stmts := []string{}
+
+    for _, change := range d.Changes {
+        switch change.Type {
+
+        case diff.AddTable:
+            stmts = append(stmts, fmt.Sprintf("CREATE TABLE %s ();", change.TableName))
+
+        case diff.DropTable:
+            stmts = append(stmts, fmt.Sprintf("DROP TABLE IF EXISTS %s CASCADE;", change.TableName))
+
+        case diff.AddColumn:
+            stmts = append(stmts, fmt.Sprintf(
+                "ALTER TABLE %s ADD COLUMN %s %s;",
+                change.TableName, change.Column.Name, change.Column.Type,
+            ))
+
+        case diff.DropColumn:
+            stmts = append(stmts, fmt.Sprintf(
+                "ALTER TABLE %s DROP COLUMN %s;",
+                change.TableName, change.Column.Name,
+            ))
+
+        case diff.ModifyColumn:
+            stmts = append(stmts, fmt.Sprintf(
+                "ALTER TABLE %s ALTER COLUMN %s TYPE %s;",
+                change.TableName, change.Column.Name, change.Column.Type,
+            ))
+        }
+    }
+
+    return stmts, nil
+}
