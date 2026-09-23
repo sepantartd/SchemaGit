09bd04032d861583c3e0e4a62efe0b6acb095f2d
Sepanta <spnta40@gmail.com>
2026-09-24 01:40:58 +0330

    Add project agent logs route

--- internal/cloud/routes.go
+++ internal/cloud/routes.go
@@ -1,1 +1,17 @@
+package cloud
 
+import (
+    "net/http"
+    "strconv"
+)
+
+func ProjectAgentLogs(w http.ResponseWriter, r *http.Request) {
+    pidStr := r.URL.Query().Get("project_id")
+    pid, _ := strconv.ParseInt(pidStr, 10, 64)
+
+    logs := projStore.LoadAgentLogsByProject(pid)
+
+    render(w, "project_logs_agent.html", map[string]interface{}{
+        "AgentLogs": logs,
+    })
+}
