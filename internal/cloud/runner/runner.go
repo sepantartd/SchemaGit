5c3d981b2b8c694227b5fde8e32ad14bc97645fb
Sepanta <spnta40@gmail.com>
2026-09-24 02:11:15 +0330

    Create runner.go

---
+++ internal/cloud/runner/runner.go
@@ -0,0 +1,34 @@
+package runner
+
+import "github.com/google/uuid"
+
+type Runner struct {
+    Queue    *Queue
+    Executor *Executor
+}
+
+func NewRunner(agentURL string) *Runner {
+    return &Runner{
+        Queue:    NewQueue(),
+        Executor: NewExecutor(agentURL),
+    }
+}
+
+func (r *Runner) RunMigration(projectID string, plan []string) (*Job, error) {
+    job := Job{
+        ID:        uuid.New().String(),
+        ProjectID: projectID,
+        Plan:      plan,
+        Status:    Pending,
+    }
+
+    r.Queue.Add(job)
+
+    next := r.Queue.Next()
+    if next == nil {
+        return nil, nil
+    }
+
+    err := r.Executor.Execute(next)
+    return next, err
+}
