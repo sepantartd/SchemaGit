8353a4e865d7701acee230ea6166943dff224e97
Sepanta <spnta40@gmail.com>
2026-09-24 01:37:20 +0330

    Create cloud render helper

--- internal/cloud/render.go
+++ internal/cloud/render.go
@@ -1,1 +1,23 @@
+package cloud
 
+import (
+    "html/template"
+    "net/http"
+    "path/filepath"
+)
+
+func render(w http.ResponseWriter, name string, data interface{}) {
+    path := filepath.Join("dashboard", name)
+
+    tmpl, err := template.ParseFiles(path)
+    if err != nil {
+        http.Error(w, err.Error(), 500)
+        return
+    }
+
+    err = tmpl.Execute(w, data)
+    if err != nil {
+        http.Error(w, err.Error(), 500)
+        return
+    }
+}
